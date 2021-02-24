51e8a845fbe64ec6a54cf07dd8f35c0cb93f3663
Jesse Jonas <jessejonas13@gmail.com>
2021-02-23 22:10:52 -0300

    feat: countdown funcional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
+import { CompletedChallenges } from '../components/CompletedCallenge';
+import { Countdown } from '../components/CountDown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 
-import { ExperienceBar } from "../components/ExperienceBar";
+import Head from 'next/head';
 
-export default function Home() {
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home () {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+
+        <div />
+      </section>
     </div>
-  )
+  );
 }
