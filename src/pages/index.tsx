95d5490599761669c92156830727278b42991a9d
Jesse Jonas <jessejonas13@gmail.com>
2021-02-24 22:08:57 -0300

    feat: challenge box finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css';
+
 import { CompletedChallenges } from '../components/CompletedCallenge';
-import { Countdown } from '../components/CountDown';
+import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-
-import Head from 'next/head';
-
-import styles from '../styles/pages/Home.module.css';
 
 export default function Home () {
   return (
@@ -23,7 +23,9 @@ export default function Home () {
           <Countdown />
         </div>
 
-        <div />
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
